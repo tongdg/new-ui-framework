8f876f359b766b6ead3e0b372e065479140b46e5
tongdg <892431872@qq.com>
2019-12-03 16:37:07 +0800

    '增加例子'

--- data/flow_list.py
+++ data/flow_list.py
@@ -16,7 +16,23 @@ login = ['tong', '123456', '多租户空库0814', '童定国']
 login_flow = {'login': login, }
 wait_deal_flow = {'login': login, 'wait_deal': ''}
 already_deal_flow = {'login': login, 'already_deal': ''}
+cc_task_flow = {'login': login, 'cc_task': ''}
+all_task_flow = {'login': login, 'all_task': ''}
+at_me_flow = {'login': login, 'at_me': ''}
+my_concern_flow = {'login': login, 'my_concern': ''}
+message_reminder_flow = {'login': login, 'message_reminder': ''}
 
+all_flow = {
+    'login': login,
+    'wait_deal': '',
+    'cc_task': '',
+    'already_deal': '',
+    'all_task': '',
+    'at_me': '',
+    'my_concern': '',
+    'message_reminder': '',
+
+}
 
 # mobile 流程
 mobile_login_flow = {'mobile_login': login, }
@@ -24,9 +40,15 @@ mobile_login_flow = {'mobile_login': login, }
 
 # 组装流程
 pc_flow = [
-    login_flow,
-    wait_deal_flow,
-    already_deal_flow,
+    # login_flow,
+    # wait_deal_flow,
+    # already_deal_flow,
+    # cc_task_flow,
+    # all_task_flow,
+    # my_concern_flow,
+    # message_reminder_flow,
+    all_flow,
+
 ]
 
 mobile_flow = [
--- pages/pc/wait_deal_page.py
+++ pages/pc/wait_deal_page.py
@@ -33,6 +33,56 @@ class WaitDealPage(MainTesting):
         )
         assert mark
 
+    @router(r'cc_task')
+    def cc_task(self):
+        self.pre_operation()
+        self.driver.click('抄送任务')
+        mark = self.driver.find_elements(
+            '#root > div > section > section > section > main > section > section > main > section > main > div > div'
+        )
+        assert mark
+
+    @router(r'all_task')
+    def all_task(self):
+        self.pre_operation()
+        self.driver.click('全部任务')
+        mark = self.driver.find_elements(
+            '#root > div > section > section > section > main > section > section > main > section > main > div > '
+            'div.components-todo-formcontent__record_lists--29V_wNCX'
+        )
+        assert mark
+
+    @router(r'at_me')
+    def at_me(self):
+        self.pre_operation()
+        self.driver.click('@我的')
+        mark = self.driver.find_elements(
+            '#root > div > section > section > section > main > section > section > main > section > main > div'
+        )
+        assert mark
+
+    @router('my_concern')
+    def my_concern(self):
+        self.pre_operation()
+        self.driver.click('我关注的')
+        mark = self.driver.find_elements(
+            '#root > div > section > section > section > main > section > section > main > section > main > div > div'
+        )
+        assert mark
+
+    @router('message_reminder')
+    def message_reminder(self):
+        self.pre_operation()
+        self.driver.click('消息提醒')
+        mark = self.driver.find_elements(
+            '#root > div > section > section > section > main > section > section > main > section > main > div '
+            '> div > div'
+        )
+        assert mark
+
+
+
+
 
 
 
